af5fcde57ef621ec66d4d9c435d9962ee542b9ef
makhammadsoliyev <makhammadsoliyev@gmail.com>
2023-11-24 22:24:56 +0500

    Add project files.

---
+++ practical_project1_multiplication_table/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(string.Format("{0, 90}", "Multiplication table from 1 to 10"));
+
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        /*Console.Write(string.Format("{0, -14}", $"{j} * {i} = {i * j}"));*/
+        Console.Write($"{j} * {i} = {j * i}\t");
+    }
+    Console.WriteLine();
+}
